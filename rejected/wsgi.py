aadf0cd3566211b463141413a9c72463183f73b9
arjunsoota <arjunsoota97@gmail.com>
2019-10-12 00:03:09 +0530

    deployment changes

--- rejected/wsgi.py
+++ rejected/wsgi.py
@@ -11,9 +11,7 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 from whitenoise.django import DjangoWhiteNoise
-
-application = DjangoWhiteNoise(application)
-
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'rejected.settings')
 
+application = DjangoWhiteNoise(application)
 application = get_wsgi_application()
